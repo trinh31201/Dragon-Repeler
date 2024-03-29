a024a0549e8c663bda849e0eb2c7274bf4007001
trinh31201 <141480383+trinh31201@users.noreply.github.com>
2024-03-29 12:50:43 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+	{
+		name: "stick",
+		power: 5
+	},
+	{
+		name: "dagger",
+		power: 30
+	},
+	{
+		name: "claw hammer",
+		power: 50
+	},
+	{
+		name: "sword",
+		power: 100
+	}
+];
+
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+];
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store.\""
+    },
+	{
+		name: "store",
+		"button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
